cbcab659b199fd392fd3447b7c2aa2510e0e0588
rm-a0 <michalrepcik9@gmail.com>
2024-07-27 21:51:28 +0200

    .

---
+++ shift.py
@@ -0,0 +1,9 @@
+class Shift():
+    def __init__(self, day, min_emps, max_emps):
+        self.day = day
+        self.min_emps = min_emps
+        self.max_emps = max_emps
+        self.employees = []
+
+    def add_employee(self, employee)
+        self.employees = 
