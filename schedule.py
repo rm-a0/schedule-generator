9ac29b45430c5458605438a0f327b368eb8689e3
rm-a0 <michalrepcik9@gmail.com>
2024-07-27 11:25:02 +0200

    added print_cell method and rewrote printing functions

--- schedule.py
+++ schedule.py
@@ -16,23 +16,34 @@ class Schedule():
     def get_num_days(self):
         self.num_days = calendar.monthrange(self.year, self.month)[1]
 
-    def print_day_num_row(self):
-        for i in range (1, self.num_days + 1):
-            if i < 9:
-                print(f" {i}", end = '  ')
-            else:
-                print(f" {i}", end = ' ')
-        print()
-
     def truncate_words(self, words, num_chars):
         return [word[:num_chars] for word in words]
+
+    def print_cell(self, content, width, align):
+        c_str = str(content)
+        word_len = len(c_str)
+
+        if align == "center":
+            padding = (width - word_len) // 2
+            print(' ' * padding + c_str + ' ' * (width - word_len - padding), end = ' ')
+
+        elif align == "left":
+            print(c_str + ' ' * (width - word_len), end = ' ')
+
+        elif align == "right":
+            print(' ' * (width - word_len) + c_str, end = ' ')
+
+    def print_day_num_row(self):
+        for i in range (1, self.num_days + 1):
+            self.print_cell(i, 4, "center")
+        print()
 
     def print_day_name_row(self):
         start_day = calendar.monthrange(self.year, self.month)[0]
         day_names = self.truncate_words(list(calendar.day_name), 2)
 
         for i in range (0, self.num_days):
-            print(day_names[(start_day + i) % 7], end = '  ')
+            self.print_cell(day_names[(start_day + i) % 7], 4, "center")
         print()
 
     def print_table(self):
