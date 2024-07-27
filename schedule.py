a756a75b82c4b5d9d130662817735c49c028b6d3
rm-a0 <michalrepcik9@gmail.com>
2024-07-27 21:37:29 +0200

    modified padding to be modular with month name

--- schedule.py
+++ schedule.py
@@ -34,6 +34,7 @@ class Schedule():
             print(' ' * (width - word_len) + c_str, end = '')
 
     def print_day_num_row(self):
+        self.print_cell(calendar.month_name[self.month], len(calendar.month_name[self.month]) + 2, "center")
         for i in range (1, self.num_days + 1):
             self.print_cell(i, 4, "center")
         print()
@@ -42,13 +43,15 @@ class Schedule():
         start_day = calendar.monthrange(self.year, self.month)[0]
         day_names = self.truncate_words(list(calendar.day_name), 2)
 
+        self.print_cell("", len(calendar.month_name[self.month]) + 2, "center")
+
         for i in range (0, self.num_days):
             self.print_cell(day_names[(start_day + i) % 7], 4, "center")
         print()
 
     def print_line_row(self):
-        for i in range (0, self.num_days):
-            self.print_cell("----", 4, "center")
+        for i in range (0, self.num_days*4 + len(calendar.month_name[self.month]) + 2):
+            self.print_cell("-", 1, "center")
         print()
 
     def print_table(self):
