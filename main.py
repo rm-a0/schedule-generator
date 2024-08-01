2c9de1e43bf209802aad180332c8525dcb9e3eac
rm-a0 <michalrepcik9@gmail.com>
2024-08-01 14:49:45 +0200

    todo:fix typos

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from schedule import Schedule
 from employee import Employee
+from shift import Shift 
 
 def main():
     schedule = Schedule(8, 2024, 4)
+    f = Shift("F1", 5, 6, 0.5)
     emp1 = Employee("Abcd", 23, "fulltime")
     emp2 = Employee("Efgh", 23, "fulltime")
     emp3 = Employee("Ijkl", 23, "fulltime")
